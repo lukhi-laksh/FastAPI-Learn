9c5ec5a6a88376a94c27bc3aac295e5042eabf62
lukhi-laksh <luxlukhi04@gmail.com>
2025-07-23 17:42:45 +0530

    Types in python

--- pythonTypes.py
+++ pythonTypes.py
@@ -5,3 +5,12 @@ def add(firstname: str | list, lastname: str):
 firstname = 'laksh'
 lastname = 'lukhi'
 print(add(firstname, lastname))
+
+def say_hi(name: str | None = None):
+    if name is not None:
+        print(f"Hey {name}!")
+    else:
+        print("Hello World")
+
+say_hi('laksh')
+say_hi()
